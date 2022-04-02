6aa449f013cffcfed5dc1accb24ab33b55c5093d
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-04-02 15:20:36 -0700

    Fix problems

--- app/src/main/java/com/chocolateloverraj/androidapiwebview/MainActivity.kt
+++ app/src/main/java/com/chocolateloverraj/androidapiwebview/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import com.chocolateloverraj.androidapiwebview.databinding.ActivityMain2Binding
+import com.chocolateloverraj.androidapiwebview.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private var binding: ActivityMain2Binding? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
         findViewById<BottomNavigationView>(R.id.nav_view)
         // Passing each menu ID as a set of Ids because each
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard)
                 .build()
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main)
         NavigationUI.setupWithNavController(binding!!.navView, navController)
     }
 }
