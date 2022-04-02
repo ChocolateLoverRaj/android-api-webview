ec6de69c42f44a450033d5b9203c5cdae263cb44
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-04-02 11:07:22 -0700

    Open web page in new 'document'
    This allows multiple pages to be open at the same time

--- app/src/main/java/com/chocolateloverraj/androidapiwebview/MainActivity.kt
+++ app/src/main/java/com/chocolateloverraj/androidapiwebview/MainActivity.kt
@@ -1,36 +1,26 @@
-package com.chocolateloverraj.androidapiwebview;
+package com.chocolateloverraj.androidapiwebview
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.chocolateloverraj.androidapiwebview.databinding.ActivityMain2Binding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.chocolateloverraj.androidapiwebview.databinding.ActivityMain2Binding;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ActivityMain2Binding binding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMain2Binding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        findViewById<BottomNavigationView>(R.id.nav_view)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+                .build()
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2)
+        NavigationUI.setupWithNavController(binding!!.navView, navController)
     }
-
 }
--- app/src/main/java/com/chocolateloverraj/androidapiwebview/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chocolateloverraj/androidapiwebview/ui/home/HomeFragment.kt
@@ -1,52 +1,43 @@
-package com.chocolateloverraj.androidapiwebview.ui.home;
+package com.chocolateloverraj.androidapiwebview.ui.home
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.chocolateloverraj.androidapiwebview.PageViewer
+import com.chocolateloverraj.androidapiwebview.databinding.FragmentHomeBinding
+import java.util.*
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import com.chocolateloverraj.androidapiwebview.PageViewer;
-import com.chocolateloverraj.androidapiwebview.databinding.FragmentHomeBinding;
-
-import java.util.Objects;
-
-public class HomeFragment extends Fragment {
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        binding.openButton.setOnClickListener(this::onOpen);
-
-        return binding.getRoot();
+class HomeFragment : Fragment() {
+    private var binding: FragmentHomeBinding? = null
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding!!.openButton.setOnClickListener { view: View? -> onOpen() }
+        return binding!!.root
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
-    public void onOpen(View view) {
-        System.out.println(binding.openText.getText());
-        Intent intent = new Intent(getActivity(), PageViewer.class);
-        Uri uri = Uri.parse(Objects.requireNonNull(binding.openText.getText()).toString());
-        if (uri.getScheme() == null)
-            uri = Uri.fromParts("android-api-webview", uri.getPath(), uri.getFragment());
-        else if (!uri.getScheme().equals("https")) {
-            Toast.makeText(getActivity(), "Url must be https", Toast.LENGTH_LONG).show();
-            return;
+    private fun onOpen() {
+        println(binding!!.openText.text)
+        val intent = Intent(activity, PageViewer::class.java)
+        var uri = Uri.parse(Objects.requireNonNull(binding!!.openText.text).toString())
+        if (uri.scheme == null) uri = Uri.fromParts("android-api-webview", uri.path, uri.fragment) else if (uri.scheme != "https") {
+            Toast.makeText(activity, "Url must be https", Toast.LENGTH_LONG).show()
+            return
         }
-        System.out.println(uri);
-        System.out.println(uri.getScheme() + ", " + uri.getHost() + ", " + uri.getPath() + ", " + uri.getFragment());
-        intent.setData(uri);
-        startActivity(intent);
+        println(uri)
+        println(uri.scheme + ", " + uri.host + ", " + uri.path + ", " + uri.fragment)
+        intent.data = uri
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+        startActivity(intent)
     }
 }
