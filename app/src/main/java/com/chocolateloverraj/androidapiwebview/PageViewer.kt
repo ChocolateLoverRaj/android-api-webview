73630da99294d08c869072ebef8ff5fe38c28978
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-04-04 10:10:45 -0700

    Load last url after rotation

--- app/src/main/java/com/chocolateloverraj/androidapiwebview/PageViewer.kt
+++ app/src/main/java/com/chocolateloverraj/androidapiwebview/PageViewer.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 class PageViewer : AppCompatActivity() {
     companion object {
         const val customScheme = "android-api-webview"
+        const val lastUrlId = "lastUrl"
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -19,7 +20,8 @@ class PageViewer : AppCompatActivity() {
         val intent = intent
         val uri = intent.data!!
         println(uri)
-        val webViewUrl = "https" + uri.toString().substring(uri.scheme!!.length)
+        val lastUrl = savedInstanceState?.getString(lastUrlId)
+        val webViewUrl = lastUrl ?: "https" + uri.toString().substring(uri.scheme!!.length)
         println(webViewUrl)
         webView.settings.javaScriptEnabled = true
         webView.settings.databaseEnabled = true
@@ -28,5 +30,11 @@ class PageViewer : AppCompatActivity() {
         webView.settings.blockNetworkLoads = false
         webView.webViewClient = WebViewClient()
         webView.loadUrl(webViewUrl)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val webView = findViewById<WebView>(R.id.webView)
+        outState.putString(lastUrlId, webView.url)
     }
 }
